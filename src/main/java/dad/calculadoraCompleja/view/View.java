e60fc68223b4ed3f137a71ab70e057037ff3ef39
VegasBeats <j.j.13sg10@gmail.com>
2021-11-05 09:58:47 +0000

    Version 0.0.1 Eclipse Project Created

---
+++ src/main/java/dad/calculadoraCompleja/view/View.java
@@ -0,0 +1,19 @@
+package dad.calculadoraCompleja.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class View implements Initializable {
+
+	public View() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
